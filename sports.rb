51172a95616fc66e9143bf840de6ff808592fc94
Alexandra Dresco <alexandra.d1510@gmail.com>
2018-08-19 13:44:43 +0100

    Sports

---
+++ sports.rb
@@ -0,0 +1,14 @@
+players = [
+  { :name => "Sam", :sport => "tennis" },
+  { :name => "Mary", :sport => "squash" },
+  { :name => "Ed", :sport => "tennis" },
+  { :name => "Mark", :sport => "football" }
+]
+
+players = {
+  :tennis => ["Sam","Ed"],
+  :squash => "Mary",
+  :football => "Mark"
+}
+
+puts players
